48a680a7a55be4662598bc6591c8a94634adc1f5
Julien Rolland <julien.rolland@universite-paris-saclay.fr>
2025-12-29 00:17:32 +0100

    set up tracing + adding egui

--- src/main.rs
+++ src/main.rs
@@ -1,3 +1,11 @@
-fn main() {
-    println!("Hello, world!");
+use anyhow::Result;
+use tracing_subscriber::{EnvFilter, fmt, layer::SubscriberExt, util::SubscriberInitExt};
+
+fn main() -> Result<()> {
+    tracing_subscriber::registry()
+        .with(fmt::layer())
+        .with(EnvFilter::from_default_env())
+        .try_init()?;
+
+    Ok(())
 }
